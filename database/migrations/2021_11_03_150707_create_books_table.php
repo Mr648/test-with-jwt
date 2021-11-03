ffe4dc3519ffa65ea96286fe1deea536c56b18ed
mr648 <rahmatwaisi@gmail.com>
2021-11-03 18:40:46 +0330

    added book model

---
+++ database/migrations/2021_11_03_150707_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('author_id')->constrained()->on('users')
+                ->nullOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->string('name');
+            $table->integer('pages')->default(0);
+            $table->string('publisher')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
